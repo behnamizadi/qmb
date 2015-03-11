7ab75b539be4dbf5fe38e3798dcd98e25c5f6444
behnamizadi <behnamizadi@yahoo.com>
2015-03-11 13:10:06 -0700

    scripts moved to end of main layout

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -1,9 +1,8 @@
-﻿<!DOCTYPE html>
+﻿<?php $path=PHP40::get()->homeUrl; ?>
+<!DOCTYPE html>
 <head>
 <meta charset="UTF-8">
-<link rel="stylesheet" type="text/css" href="<?php $path=PHP40::get()->homeUrl; echo $path; ?>public/css/main.css"  />
-<script type="text/javascript" src="<?php  echo $path;?>public/jscript/jquery-1.8.3.min.js"></script>
-<script src="<?php echo $path;?>public/jscript/focus.js"></script>
+<link rel="stylesheet" type="text/css" href="<?php echo $path; ?>public/css/main.css"  />
 <title>سامانه مدیریت منابع انسانی یاغیش- نسخه ۱</title>
 </head>
 <body>
@@ -14,6 +13,8 @@
                 <?php include($view); ?>
 	    </div>
 	      <?php include('footer.php'); ?>
+<script type="text/javascript" src="<?php  echo $path;?>public/jscript/jquery-1.8.3.min.js"></script>	      
+<script src="<?php echo $path;?>public/jscript/focus.js"></script>	      
 <?php if (isset($scripts)) echo $scripts; ?>	      
 </body>
 </html>
