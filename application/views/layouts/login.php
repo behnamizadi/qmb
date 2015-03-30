e015ea8db7ff910f51803d2d6c6aa8c4e3cd3fda
behnamizadi <behnamizadi@yahoo.com>
2015-03-30 03:53:03 -0700

    better display of add path

--- application/views/layouts/login.php
+++ application/views/layouts/login.php
@@ -12,6 +12,7 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <link rel="stylesheet" type="text/css" href="<?php echo $path; ?>public/css/shared.css" >
         <link rel="stylesheet" type="text/css" href="<?php echo $path; ?>public/css/main.css" >
         <title>سامانه مدیریت منابع انسانی یاغیش</title>
         <?php include 'favicon.php';?>
@@ -20,10 +21,12 @@
     <div class="background-container">
    <img class="background-image" src="<?php echo $path;?>public/images/qmbg.jpg" />
    </div><div class="container-fluid">
+       
              <h2 class="bigtitle">سامانه مدیریت منابع انسانی بانک قرض‌الحسنه مهر ایران</h2>
-              <div id="view">
+              <div class="login">
                 <?php include($view); ?>
                 </div>
+               
  </div>
    <!-- container -->
 <script type="text/javascript" src="<?php echo $path; ?>public/jscript/jquery.min.js"></script>
