09876f59e31799386c7ac46bd42383366a3d7304
behnamizadi <behnamizadi@yahoo.com>
2015-04-10 11:55:25 -0700

    alt tag corrected for delete icon

--- application/controllers/vacationController.php
+++ application/controllers/vacationController.php
@@ -316,7 +316,7 @@ class vacationController {
                 $end += 86400;
             $v -> condition = "WHERE date_start BETWEEN $f AND $end AND clerk_id='$clerk_id'";
         } else {$v -> condition = "WHERE clerk_id='$clerk_id'";
-        }$v -> operations = array('view' => FALSE, 'edit' => FALSE, 'delete' => FALSE, 'vacation/edit/$value->id/' . $clerk_id . '/' . $year => array('icon' => 'public/images/edit.png', 'alt' => 'ویرایش', 'title' => 'ویرایش'), 'vacation/delete/$value->id/' . $clerk_id . '/' . $year => array('icon' => 'public/images/delete.png', 'alt' => 'ویرایش', 'title' => 'ویرایش'), );
+        }$v -> operations = array('view' => FALSE, 'edit' => FALSE, 'delete' => FALSE, 'vacation/edit/$value->id/' . $clerk_id . '/' . $year => array('icon' => 'public/images/edit.png', 'alt' => 'ویرایش', 'title' => 'ویرایش'), 'vacation/delete/$value->id/' . $clerk_id . '/' . $year => array('icon' => 'public/images/delete.png', 'alt' => 'حذف', 'title' => 'حذف'), );
         $v -> sort = 'hokm_number DESC';
         $v -> headers = array('date_added' => array('format' => 'model[Cal,getDate($value)]', 'label' => 'تاریخ حکم'), 'hokm_number', 'date_start' => array('format' => 'model[Cal,getDate($value)]', 'label' => 'تاریخ شروع'), 'date_end' => array('format' => 'model[Cal,getDate($value)]', 'label' => 'تاریخ پایان'), 'period', 'type' => array('format' => 'model[Lookup,getById($value,vacation)]', ), 'description');
         $a = new CView;
