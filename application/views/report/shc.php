cf343d63f7de4ae718dbc5407094a87d82c822bd
behnamizadi <behnamizadi@yahoo.com>
2015-04-10 06:33:44 -0700

    table fixed to show all posts

--- application/views/report/shc.php
+++ application/views/report/shc.php
@@ -1,4 +1,7 @@
-<?php if(isset($ptitle)) echo $ptitle; ?>
+<?php
+if (isset($ptitle))
+    echo $ptitle;
+ ?>
 <?php
 $lookup = new Lookup;
 $geos = $lookup -> getAll('geo');
@@ -31,90 +34,87 @@ $geos = $lookup -> getAll('geo');
 		?>
 		<tr>
 			<td><?php echo $geos[$geo]; ?></td>
-			<td><?php echo $branch; $bCount += $branch; ?></td>
+			<td><?php echo $branch;
+                $bCount += $branch;
+ ?></td>
 			<td>
 				<?php
-				if(isset($clerkCount[$geo])) 
-				{
-					$cCount += $clerkCount[$geo];
-					echo $clerkCount[$geo];
-				}
-				else
-					echo '۰'; 
+                if (isset($clerkCount[$geo])) {
+                    $cCount += $clerkCount[$geo];
+                    echo $clerkCount[$geo];
+                } else
+                    echo '۰';
 				?>
 			</td>
 			<td>
-				<?php 
-				if(isset($bDegrees[$geo][7]))
-				{
-					echo $bDegrees[$geo][7];
-					$bJam[7] += $bDegrees[$geo][7];
-				}
+				<?php
+                if (isset($bDegrees[$geo][7])) {
+                    echo $bDegrees[$geo][7];
+                    $bJam[7] += $bDegrees[$geo][7];
+                }
 				?>
 			</td>
 			<td>
-				<?php 
-				if(isset($bDegrees[$geo][6]))
-				{
-					echo $bDegrees[$geo][6];
-					$bJam[6] += $bDegrees[$geo][6];
-				}
+				<?php
+                if (isset($bDegrees[$geo][6])) {
+                    echo $bDegrees[$geo][6];
+                    $bJam[6] += $bDegrees[$geo][6];
+                }
 				?>
 			</td>
 			<td>
-				<?php 
-				if(isset($bDegrees[$geo][1]))
-				{
-					echo $bDegrees[$geo][1];
-					$bJam[1] += $bDegrees[$geo][1];
-				}
+				<?php
+                if (isset($bDegrees[$geo][1])) {
+                    echo $bDegrees[$geo][1];
+                    $bJam[1] += $bDegrees[$geo][1];
+                }
 				?>
 			</td>
 			<td>
-				<?php 
-				if(isset($bDegrees[$geo][2]))
-				{
-					echo $bDegrees[$geo][2];
-					$bJam[2] += $bDegrees[$geo][2];
-				}
+				<?php
+                if (isset($bDegrees[$geo][2])) {
+                    echo $bDegrees[$geo][2];
+                    $bJam[2] += $bDegrees[$geo][2];
+                }
 				?>
 			</td>
 			<td>
-				<?php 
-				if(isset($bDegrees[$geo][3]))
-				{
-					echo $bDegrees[$geo][3];
-					$bJam[3] += $bDegrees[$geo][3];
-				}
+				<?php
+                if (isset($bDegrees[$geo][3])) {
+                    echo $bDegrees[$geo][3];
+                    $bJam[3] += $bDegrees[$geo][3];
+                }
 				?>
 			</td>
 			<td>
-				<?php 
-				if(isset($bDegrees[$geo][4]))
-				{
-					echo $bDegrees[$geo][4];
-					$bJam[4] += $bDegrees[$geo][4];
-				}
+				<?php
+                if (isset($bDegrees[$geo][4])) {
+                    echo $bDegrees[$geo][4];
+                    $bJam[4] += $bDegrees[$geo][4];
+                }
 				?>
 			</td>
 			<td>
-				<?php 
-				if(isset($bDegrees[$geo][5]))
-				{
-					echo $bDegrees[$geo][5];
-					$bJam[5] += $bDegrees[$geo][5];
-				}
+				<?php
+                if (isset($bDegrees[$geo][5])) {
+                    echo $bDegrees[$geo][5];
+                    $bJam[5] += $bDegrees[$geo][5];
+                }
 				?>
 			</td>
 		</tr>
 		<?php
-		$i++;
-		endforeach;
+        $i++;
+        endforeach;
 		?>
-		<tr class="<?php echo $i%2 == 0 ? 'even' : 'odd'; $i++; ?>">
+		<tr class="<?php echo $i % 2 == 0 ? 'even' : 'odd';
+            $i++;
+ ?>">
 			<td>سرپرستی</td>
 			<td>۰</td>
-			<td><?php echo $sarCount; $cCount += $sarCount; ?></td>
+			<td><?php echo $sarCount;
+                $cCount += $sarCount;
+ ?></td>
 			<td>&nbsp;</td>
 			<td>&nbsp;</td>
 			<td>&nbsp;</td>
@@ -189,8 +189,8 @@ $eStatuses = $lookup -> getAll('employment_status');
 				<td><?php echo isset($sex[$code][1]) ? $sex[$code][1] : '۰'; ?></td>
 			</tr>
 		<?php
-		$i++;
-		endforeach;
+        $i++;
+        endforeach;
 		?>
 		<tr>
 			<td>جمع</td>
@@ -202,255 +202,30 @@ $eStatuses = $lookup -> getAll('employment_status');
 		</tr>
 		</tbody>
 </table>
-<?php 
-$postCount = count($posts);
-$tblNumber = 3;
-$tblWidth = floor(100/$tblNumber) - 1;
-$marginLeft = (100 - ($tblWidth * $tblNumber)) / ($tblNumber - 1);
-$maxTR = ceil($postCount/$tblNumber);
-$index = 1;
-for($i = 0; $i < $tblNumber; $i++):
-?>
-	<table class="table table-striped table-bordered" style="width:<?php echo $tblWidth; ?>%;<?php if(($tblNumber-$i) == 1) echo 'float:left;'; else{ echo 'float:right;'; echo 'margin-left:'; echo $marginLeft; echo '%;'; } ?>margin-top:20px;">
+	<table class="table table-striped table-bordered">
 		<tr>
-			<th style="width: 10%;">ردیف</th>
-			<th style="width: 75%;">پست</th>
+			<th>ردیف</th>
+			<th>پست</th>
 			<th style="width: 15%;">تعداد</th>
 		</tr>
-			<?php 
-			for($x = 1; $x <= $postCount; $x++)
-			{
-				$tmp = 1;
-				if(isset($posts[28]) && $posts[28]['set'] == 0)
-				{
-					echo '<tr><td>'.$index.'</td><td>'.$posts[28]['name'].'</td><td>'.$posts[28]['count'].'</tr>';
-					$posts[28]['set'] = 1;
-					$index++;
-					$tmp++;
-					if($tmp > $maxTR)
-						break;
-				}
-				if(isset($posts[30]) && $posts[30]['set'] == 0)
-				{
-					
-					echo '<tr><td>'.$index.'</td><td>'.$posts[30]['name'].'</td><td>'.$posts[30]['count'].'</tr>';
-					$posts[30]['set'] = 1;
-					$index++;
-					$tmp++;
-					if($tmp > $maxTR)
-						break;
-				}
-				if(isset($posts[21]) && $posts[21]['set'] == 0)
-				{
-				
-					echo '<tr><td>'.$index.'</td><td>'.$posts[21]['name'].'</td><td>'.$posts[21]['count'].'</tr>';
-					$posts[21]['set'] = 1;
-					$index++;
-					$tmp++;
-					if($tmp > $maxTR)
-						break;
-				}
-				if(isset($posts[12]) && $posts[12]['set'] == 0)
-				{
-					echo '<tr><td>'.$index.'</td><td>'.$posts[12]['name'].'</td><td>'.$posts[12]['count'].'</tr>';
-					$posts[12]['set'] = 1;
-					$index++;
-					$tmp++;
-					if($tmp > $maxTR)
-						break;
-				}
-				if(isset($posts[13]) && $posts[13]['set'] == 0)
-				{
-					
-					echo '<tr ><td>'.$index.'</td><td>'.$posts[13]['name'].'</td><td>'.$posts[13]['count'].'</tr>';
-					$posts[13]['set'] = 1;
-					$index++;
-					$tmp++;
-					if($tmp > $maxTR)
-						break;
-				}
-				if(isset($posts[14]) && $posts[14]['set'] == 0)
-				{
-					
-					echo '<tr ><td>'.$index.'</td><td>'.$posts[14]['name'].'</td><td>'.$posts[14]['count'].'</tr>';
-					$posts[14]['set'] = 1;
-					$index++;
-					$tmp++;
-					if($tmp > $maxTR)
-						break;
-				}
-				if(isset($posts[20]) && $posts[20]['set'] == 0)
-				{
-					
-					echo '<tr><td>'.$index.'</td><td>'.$posts[20]['name'].'</td><td>'.$posts[20]['count'].'</tr>';
-					$posts[20]['set'] = 1;
-					$index++;
-					$tmp++;
-					if($tmp > $maxTR)
-						break;
-				}
-				if(isset($posts[29]) && $posts[29]['set'] == 0)
-				{
-					echo '<tr ><td>'.$index.'</td><td>'.$posts[29]['name'].'</td><td>'.$posts[29]['count'].'</tr>';
-					$posts[29]['set'] = 1;
-					$index++;
-					$tmp++;
-					if($tmp > $maxTR)
-						break;
-				}
-				if(isset($posts[15]) && $posts[15]['set'] == 0)
-				{
-					
-					echo '<tr><td>'.$index.'</td><td>'.$posts[15]['name'].'</td><td>'.$posts[15]['count'].'</tr>';
-					$posts[15]['set'] = 1;
-					$index++;
-					$tmp++;
-					if($tmp > $maxTR)
-						break;
-				}
-				if(isset($posts[2]) && $posts[2]['set'] == 0)
-				{
-				
-					echo '<tr><td>'.$index.'</td><td>'.$posts[2]['name'].'</td><td>'.$posts[2]['count'].'</tr>';
-					$posts[2]['set'] = 1;
-					$index++;
-					$tmp++;
-					if($tmp > $maxTR)
-						break;
-				}
-				if(isset($posts[27]) && $posts[27]['set'] == 0)
-				{
-					$class = $tmp%2 == 0 ? 'even' : 'odd'; 
-					echo '<tr class="'.$class.'"><td>'.$index.'</td><td>'.$posts[27]['name'].'</td><td>'.$posts[27]['count'].'</tr>';
-					$posts[27]['set'] = 1;
-					$index++;
-					$tmp++;
-					if($tmp > $maxTR)
-						break;
-				}
-				if(isset($posts[22]) && $posts[22]['set'] == 0)
-				{
-					$class = $tmp%2 == 0 ? 'even' : 'odd'; 
-					echo '<tr class="'.$class.'"><td>'.$index.'</td><td>'.$posts[22]['name'].'</td><td>'.$posts[22]['count'].'</tr>';
-					$posts[22]['set'] = 1;
-					$index++;
-					$tmp++;
-					if($tmp > $maxTR)
-						break;
-				}
-				if(isset($posts[18]) && $posts[18]['set'] == 0)
-				{
-					$class = $tmp%2 == 0 ? 'even' : 'odd'; 
-					echo '<tr class="'.$class.'"><td>'.$index.'</td><td>'.$posts[18]['name'].'</td><td>'.$posts[18]['count'].'</tr>';
-					$posts[18]['set'] = 1;
-					$index++;
-					$tmp++;
-					if($tmp > $maxTR)
-						break;
-				}
-				if(isset($posts[19]) && $posts[19]['set'] == 0)
-				{
-					$class = $tmp%2 == 0 ? 'even' : 'odd'; 
-					echo '<tr class="'.$class.'"><td>'.$index.'</td><td>'.$posts[19]['name'].'</td><td>'.$posts[19]['count'].'</tr>';
-					$posts[19]['set'] = 1;
-					$index++;
-					$tmp++;
-					if($tmp > $maxTR)
-						break;
-				}
-				if(isset($posts[25]) && $posts[25]['set'] == 0)
-				{
-					$class = $tmp%2 == 0 ? 'even' : 'odd'; 
-					echo '<tr class="'.$class.'"><td>'.$index.'</td><td>'.$posts[25]['name'].'</td><td>'.$posts[25]['count'].'</tr>';
-					$posts[25]['set'] = 1;
-					$index++;
-					$tmp++;
-					if($tmp > $maxTR)
-						break;
-				}
-				if(isset($posts[3]) && $posts[3]['set'] == 0)
-				{
-					$class = $tmp%2 == 0 ? 'even' : 'odd'; 
-					echo '<tr class="'.$class.'"><td>'.$index.'</td><td>'.$posts[3]['name'].'</td><td>'.$posts[3]['count'].'</tr>';
-					$posts[3]['set'] = 1;
-					$index++;
-					$tmp++;
-					if($tmp > $maxTR)
-						break;
-				}
-				if(isset($posts[23]) && $posts[23]['set'] == 0)
-				{
-					$class = $tmp%2 == 0 ? 'even' : 'odd'; 
-					echo '<tr class="'.$class.'"><td>'.$index.'</td><td>'.$posts[23]['name'].'</td><td>'.$posts[23]['count'].'</tr>';
-					$posts[23]['set'] = 1;
-					$index++;
-					$tmp++;
-					if($tmp > $maxTR)
-						break;
-				}
-				if(isset($posts[26]) && $posts[26]['set'] == 0)
-				{
-					$class = $tmp%2 == 0 ? 'even' : 'odd'; 
-					echo '<tr class="'.$class.'"><td>'.$index.'</td><td>'.$posts[26]['name'].'</td><td>'.$posts[26]['count'].'</tr>';
-					$posts[26]['set'] = 1;
-					$index++;
-					$tmp++;
-					if($tmp > $maxTR)
-						break;
-				}
-				if(isset($posts[16]) && $posts[16]['set'] == 0)
-				{
-					$class = $tmp%2 == 0 ? 'even' : 'odd'; 
-					echo '<tr class="'.$class.'"><td>'.$index.'</td><td>'.$posts[16]['name'].'</td><td>'.$posts[16]['count'].'</tr>';
-					$posts[16]['set'] = 1;
-					$index++;
-					$tmp++;
-					if($tmp > $maxTR)
-						break;
-				}
-				if(isset($posts[24]) && $posts[24]['set'] == 0)
-				{
-					$class = $tmp%2 == 0 ? 'even' : 'odd'; 
-					echo '<tr class="'.$class.'"><td>'.$index.'</td><td>'.$posts[24]['name'].'</td><td>'.$posts[24]['count'].'</tr>';
-					$posts[24]['set'] = 1;
-					$index++;
-					$tmp++;
-					if($tmp > $maxTR)
-						break;
-				}
-				if(isset($posts[31]) && $posts[31]['set'] == 0)
-				{
-					$class = $tmp%2 == 0 ? 'even' : 'odd'; 
-					echo '<tr class="'.$class.'"><td>'.$index.'</td><td>'.$posts[31]['name'].'</td><td>'.$posts[31]['count'].'</tr>';
-					$posts[31]['set'] = 1;
-					$index++;
-					$tmp++;
-					if($tmp > $maxTR)
-						break;
-				}
-				if(isset($posts[1]) && $posts[1]['set'] == 0)
-				{
-					$class = $tmp%2 == 0 ? 'even' : 'odd'; 
-					echo '<tr class="'.$class.'"><td>'.$index.'</td><td>'.$posts[1]['name'].'</td><td>'.$posts[1]['count'].'</tr>';
-					$posts[1]['set'] = 1;
-					$index++;
-					$tmp++;
-					if($tmp > $maxTR)
-						break;
-				}
-				if(isset($posts[17]) && $posts[17]['set'] == 0)
-				{
-					$class = $tmp%2 == 0 ? 'even' : 'odd'; 
-					echo '<tr class="'.$class.'"><td>'.$index.'</td><td>'.$posts[17]['name'].'</td><td>'.$posts[17]['count'].'</tr>';
-					$posts[17]['set'] = 1;
-					$index++;
-					$tmp++;
-					if($tmp > $maxTR)
-						break;
-				}
-			}
+			<?php
+            $index = 1;
+            foreach ($posts as $post) {
+                $tmp = 1;
+                if (isset($post) && $post['set'] == 0) {
+                    echo '<tr><td>' . $index . '</td><td>' . $post['name'] . '</td><td>' . $post['count'] . '</tr>';
+                    $posts[28]['set'] = 1;
+                    $index += 1;
+                }
+
+            }
 			?>
 	</table>
-<?php endfor; ?>
-<?php if(isset($pb)) echo $pb; ?>
-<?php if(isset($producer)) echo $producer; ?>
+<?php
+        if (isset($pb))
+            echo $pb;
+ ?>
+<?php
+    if (isset($producer))
+        echo $producer;
+ ?>
