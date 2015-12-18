b5abbb63bfcb2df54965d0cea69f750752c5948d
behnamizadi <behnamizadi@yahoo.com>
2015-12-18 17:14:50 +0330

    base infromation fixed to view correct titles

--- application/controllers/lookupController.php
+++ application/controllers/lookupController.php
@@ -1,44 +1,46 @@
 <?php
 class lookupController {
-    private $a = array('post' => 'پست‌های سازمانی', 'employment_status' => 'وضعیت‌های استخدامی', 'job_status' => 'وضعیت‌های اشتغال', 'sarbazi' => 'عناوین سربازی', 'geo' => 'مناطق جغرافیایی', 'degree' => 'درجات', 'study_degree' => 'مقاطع تحصیلی', 'study_field' => 'رشته‌های تحصیلی', 'vacation' => 'انواع مرخصی', 'hokm_type' => 'انواع حکم استخدامی', 't_cat' => 'رده دوره‌های آموزشی', 'training' => 'دوره‌های آموزشی', 'notice_post' => 'سمت‌های دارای اخطار');
-    private $b = array('post' => 'افزودن پست سازمانی', 'employment_status' => 'افزودن وضعیت استخدامی', 'job_status' => 'افزودن وضعیت اشتغال', 'sarbazi' => 'افزودن عنوان سربازی', 'geo' => 'افزودن منطقه جغرافیایی', 'degree' => 'افزودن درجه', 'study_degree' => 'افزودن مقطع تحصیلی', 'study_field' => 'افزودن رشته تحصیلی', 'vacation' => 'افزودن عنوان مرخصی', 'hokm_type' => 'افزودن نوع حکم', 't_cat' => 'افزودن رده دوره', 'training' => 'افزودن دوره آموزشی', 'notice_post' => 'افزودن سمت دارای اخطار');
-    private $c = array('post' => 'جزئیات پست سازمانی', 'employment_status' => 'جزئیات وضعیت استخدامی', 'job_status' => 'جزئیات وضعیت اشتغال', 'sarbazi' => 'جزئیات عنوان سربازی', 'geo' => 'جزئیات منطقه جغرافیایی', 'degree' => 'جزئیات درجه', 'study_degree' => 'جزئیات مقطع تحصیلی', 'study_field' => 'جزئیات رشته تحصیلی', 'vacation' => 'جزئیات عنوان مرخصی', 'hokm_type' => 'جزئیات نوع حکم', 't_cat' => 'جزئیات رده آموزشی', 'training' => 'جزپیات دوره آموزشی', 'notice_post' => 'جزئیات سمت دارای اخطار');
+    private $manage = array('post' => 'پست‌های سازمانی', 'employment_status' => 'وضعیت‌های استخدامی', 'job_status' => 'وضعیت‌های اشتغال', 'sarbazi' => 'عناوین سربازی', 'geo' => 'مناطق جغرافیایی', 'degree' => 'درجات', 'study_degree' => 'مقاطع تحصیلی', 'study_field' => 'رشته‌های تحصیلی', 'vacation' => 'انواع مرخصی', 'hokm_type' => 'انواع حکم استخدامی', 't_cat' => 'رده دوره‌های آموزشی', 'training' => 'دوره‌های آموزشی', 'notice_post' => 'سمت‌های دارای اخطار');
+    private $add = array('post' => 'افزودن پست سازمانی', 'employment_status' => 'افزودن وضعیت استخدامی', 'job_status' => 'افزودن وضعیت اشتغال', 'sarbazi' => 'افزودن عنوان سربازی', 'geo' => 'افزودن منطقه جغرافیایی', 'degree' => 'افزودن درجه', 'study_degree' => 'افزودن مقطع تحصیلی', 'study_field' => 'افزودن رشته تحصیلی', 'vacation' => 'افزودن عنوان مرخصی', 'hokm_type' => 'افزودن نوع حکم', 't_cat' => 'افزودن رده دوره', 'training' => 'افزودن دوره آموزشی', 'notice_post' => 'افزودن سمت دارای اخطار');
+    private $view = array('post' => 'جزئیات پست سازمانی', 'employment_status' => 'جزئیات وضعیت استخدامی', 'job_status' => 'جزئیات وضعیت اشتغال', 'sarbazi' => 'جزئیات عنوان سربازی', 'geo' => 'جزئیات منطقه جغرافیایی', 'degree' => 'جزئیات درجه', 'study_degree' => 'جزئیات مقطع تحصیلی', 'study_field' => 'جزئیات رشته تحصیلی', 'vacation' => 'جزئیات عنوان مرخصی', 'hokm_type' => 'جزئیات نوع حکم', 't_cat' => 'جزئیات رده آموزشی', 'training' => 'جزپیات دوره آموزشی', 'notice_post' => 'جزئیات سمت دارای اخطار');
     public function add() {
-        $d = CUrl::segment(3);
+        $type = CUrl::segment(3);
         $e = new CForm();
         $e -> dontClose = TRUE;
-        if (isset($_POST['submit']) && !empty($d)) {
-            if ($e -> validate() === TRUE) {$f = new Lookup;
-                $f -> add($d);
-                CUrl::redirect('lookup/manage/' . $d);
+        if (isset($_POST['submit']) && !empty($type)) {
+            if ($e -> validate() === TRUE) {
+				$f = new Lookup;
+                $f -> add($type);
+                CUrl::redirect('lookup/manage/' . $type);
             }
-        }$c = new CView;
-        $c -> type = $d;
+        }
+        $c = new CView;
+        $c -> type = $type;
         $c -> form = $e -> run();
-        if (isset($this -> add[$d]))
-            $c -> title = $this -> add[$d];
+        if (isset($this -> add[$type]))
+            $c -> title = $this -> add[$type];
         $c -> run('lookup/add');
     }
 
-    public function manage() {$d = CUrl::segment(3);
-        $g = new CGrid($d);
+    public function manage() {$type = CUrl::segment(3);
+        $g = new CGrid($type);
         $g -> headers = array('name', 'description');
         $g -> table = 'tbl_lookup';
         $g -> counter = TRUE;
-        $g -> condition = array('type' => $d);
-        $g -> operations = array('edit' => FALSE, 'view' => FALSE, 'delete' => FALSE, "lookup/view/$d/" . '$value->id' => array('icon' => 'public/images/view.png', 'alt' => 'مشاهده', 'title' => 'مشاهده و ویرایش'), "lookup/delete/$d/" . '$value->id' => array('icon' => 'public/images/delete.png', 'alt' => 'حذف', 'title' => 'حذف'));
+        $g -> condition = array('type' => $type);
+        $g -> operations = array('edit' => FALSE, 'view' => FALSE, 'delete' => FALSE, "lookup/view/$type/" . '$value->id' => array('icon' => 'public/images/view.png', 'alt' => 'مشاهده', 'title' => 'مشاهده و ویرایش'), "lookup/delete/$type/" . '$value->id' => array('icon' => 'public/images/delete.png', 'alt' => 'حذف', 'title' => 'حذف'));
         $c = new CView;
         $c -> body = $g -> run();
-        $c -> type = $d;
+        $c -> type = $type;
         $c -> add = "افزودن" ;
-        if (isset($this -> manage[$d]))
-            $c -> title = $this -> manage[$d];
-        if (isset($this -> add[$d]))
-            $c -> add = $this -> add[$d];
+        if (isset($this -> manage[$type]))
+            $c -> title = $this -> manage[$type];
+        if (isset($this -> add[$type]))
+            $c -> add = $this -> add[$type];
         $c -> run('lookup/manage');
     }
 
-    public function view() {$d = CUrl::segment(3);
+    public function view() {$type = CUrl::segment(3);
         $h = CUrl::segment(4);
         $i = new CDatabase;
         $j = $i -> getByPk($h);
@@ -49,23 +51,23 @@ class lookupController {
         if (isset($_POST['submit'])) {
             if ($e -> validate() === TRUE) {$k = array('where' => array('id' => $h));
                 $i -> update($k);
-                CUrl::redirect('lookup/manage/' . $d);
+                CUrl::redirect('lookup/manage/' . $type);
             }
         }$c = new CView;
         $c -> model = $j;
         $c -> form = $e -> run();
-        $c -> type = $d;
-        if (isset($this -> view[$d]))
-            $c -> title = $this -> view[$d];
+        $c -> type = $type;
+        if (isset($this -> view[$type]))
+            $c -> title = $this -> view[$type];
         $c -> run('lookup/add');
     }
 
-    public function delete() {$d = CUrl::segment(3);
+    public function delete() {$type = CUrl::segment(3);
         $h = CUrl::segment(4);
         $l = 'DELETE FROM tbl_lookup WHERE id="' . $h . '"';
         $i = new CDatabase;
         $i -> execute($l);
-        CUrl::redirect('lookup/manage/' . $d);
+        CUrl::redirect('lookup/manage/' . $type);
     }
 
 }
