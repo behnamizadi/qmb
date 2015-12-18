ebed29a4cd3ec478cde95a4f150efec42b629fa8
behnamizadi <behnamizadi@yahoo.com>
2015-12-18 17:02:18 +0330

    description field is converted to optional

--- application/config/form/lookup.php
+++ application/config/form/lookup.php
@@ -1,5 +1,5 @@
 <?php
 return array(
-    'add' => array('name' => array('type' => 'text', 'in' => 'class="txt" maxlength="40"', 'validation' => array('required,maxLength[40],unique[tbl_lookup]'), ), 'description' => array('type' => 'textarea', 'rows' => 10, 'cols' => 60, 'validation' => array('required'), ), 'submit' => array('type' => 'submit', 'value' => 'ثبت', 'in' => 'class="box"')), 
+    'add' => array('name' => array('type' => 'text', 'in' => 'class="txt" maxlength="40"', 'validation' => array('required,maxLength[40],unique[tbl_lookup]'), ), 'description' => array('type' => 'textarea', 'rows' => 10, 'cols' => 60 ), 'submit' => array('type' => 'submit', 'value' => 'ثبت', 'in' => 'class="box"')), 
     'view' => array('reference' => 'model', 'name' => array('type' => 'text', 'in' => 'class="txt" maxlength="40"', 'validation' => array('required,maxLength[40]'), ), 'description' => array('type' => 'textarea', 'rows' => 10, 'cols' => 60),  'submit' => array('type' => 'submit', 'value' => 'ویرایش', 'in' => 'class="box"', 'decoration' => FALSE),) );
 ?>
